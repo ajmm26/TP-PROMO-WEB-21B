0b1cb0ed2f961585808ce8158a861c62915fc61d
RaulLuppini <raulluppini@gmail.com>
2025-09-23 21:02:50 -0300

    Clase acceso datos, archivo,Marca

---
+++ TP-PROMO-WEB-21B/TP-PROMO-WEB-21B/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TP_PROMO_WEB_21B.Global" Language="C#" %>
---
+++ TP-PROMO-WEB-21B/TP-PROMO-WEB-21B/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace TP_PROMO_WEB_21B
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
